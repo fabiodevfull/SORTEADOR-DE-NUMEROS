acfad9f12fcd2f7b8dbcca7b3bc2e02a31c96eb4
Fabio Henriquem <futurodevfabiohenrique@gmail.com>
2024-03-03 22:53:07 -0300

    Teste

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Abaixo temos uma função "sortear", onde temos três vareaveis descritas como "quantidade", "de" e "ate", cada uma está buscando o id no HTML com o "document.getElementById" querendo o "value", valor do input
-// Rodamos alguns "Alert", para saber como está "alert (`Quantidade: ${quantidade}`);""
+// Rodamos alguns "Alert", para saber como está "alert (`Quantidade: ${quantidade}`);"
 function sortear() {
     let quantidade = document.getElementById ('quantidade').value;
     let de = document.getElementById ('de').value;
